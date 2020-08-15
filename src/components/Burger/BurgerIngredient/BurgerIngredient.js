42d865a61dec4ed2c671de4b82116083e5c4b737
Majid <mohammadmajid55@gmail.com>
2020-08-14 22:47:58 -0400

    adding the ingredients from the burger builder state and rendering on the Ui dynamically

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -26,8 +26,8 @@ const burgerIngredient = (props) => {
     case('salad'):
        ingredient = <div className={classes.Salad}></div>;
        break; 
-    case('bacon'):
-       ingredient = <div className={classes.Bacon}></div>;
+    case('turkeyBacon'):
+       ingredient = <div className={classes.TurkeyBacon}></div>;
        break; 
     default:
       ingredient = null;
