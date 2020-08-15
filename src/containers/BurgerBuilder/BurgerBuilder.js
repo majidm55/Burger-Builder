9f53004f97b4a1a9624906439bad43af1fad676d
Majid <mohammadmajid55@gmail.com>
2020-08-15 02:15:28 -0400

    removing the modal and backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,10 @@ class BurgerBuilder extends Component {
     this.setState({shouldOrder: true});
   }
 
+  shouldCancelOrderHandler = () => {
+    this.setState({shouldOrder: false});
+  }
+
   render () {
     const disableInfo = {
       ...this.state.ingredients
@@ -88,7 +92,10 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.shouldOrder}>
+        <Modal 
+        show={this.state.shouldOrder}
+        modalClosed={this.shouldCancelOrderHandler}
+        >
           <OrderSummary  ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
