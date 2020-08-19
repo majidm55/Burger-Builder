cf69e04f13832edaadb6c2bab65ef9f326f7cd5b
Majid <mohammadmajid55@gmail.com>
2020-08-18 21:03:56 -0400

    retreieving and rendering orders fromt he database

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
+const order = (props) => {
+  const ingredients = [];
+
+  for (let ingr in props.ingredients) {
+    ingredients.push({
+      name: ingr, 
+      amount: props.ingredients[ingr]
+    })
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+  return <span key={ig.name}
+    style={{
+      textTransform: 'capitalize',
+      display: 'inline-block',
+      margin: '0 8px',
+      border: '1px solid #ccc',
+      padding: '5px'
+    }}
+    >
+      {ig.name} ({ig.amount})</span>
+  })
+  return (
   <div className={classes.Order}>
-    <p>Ingredients: Salad(1)</p>
-    <p>Price: <strong>USD 5.45</strong></p>
+    <p>Ingredients: {ingredientOutput}</p>
+    <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
   </div>
-);
+
+  )
+};
 
 export default order;
